35e748e9e6c23b62eb89adad035ff5861c1c8280
Manmeet2018 <manietdavv@gmail.com>
2020-12-21 00:39:48 +0530

    feat: Added solution for code root

--- array-cube-root-to-json/arrayCubeRootToJson.js
+++ array-cube-root-to-json/arrayCubeRootToJson.js
@@ -1,3 +1,21 @@
-const arrayCubeRootToJson = arr => {};
+const arrayCubeRootToJson = arr => {
+    if(arr == {} || arr == undefined || arr == true || arr == false) 
+        throw new Error('Please enter array with size greater than 0');
+    let ans = {};
+    arr.forEach(element => {
+        if(typeof element !== "number")
+            throw new Error('Please enter array of all numbers');
+        ans[element] = Math.cbrt(element);
+    });
+    return ans;
+};
 
-export { arrayCubeRootToJson };
+// export { arrayCubeRootToJson };
+
+
+try{
+    console.log(arrayCubeRootToJson([1,2,3,12,3]));
+}
+catch(err) {
+    console.log(err);
+}
